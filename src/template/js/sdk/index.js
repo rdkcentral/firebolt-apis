8e14925f1b7cf80791a948d998eaaa76757e7f3d
LaCivita, Jeremy <jeremy.lacivita@comcast.com>
2021-08-11 16:53:08 -0400

    Discovery Tweaks

---
+++ src/template/js/sdk/index.js
@@ -0,0 +1,18 @@
+/*
+ * If not stated otherwise in this file or this component's LICENSE file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2021 Comcast
+ *
+ * COMCAST HIGHLY CONFIDENTIAL AND PROPRIETARY
+ * 
+ * This file and its contents are the intellectual property of Comcast and may
+ * not be used, copied, distributed or otherwise disclosed in whole or in part
+ * without the express written permission of Comcast.
+ */
+
+/* ${EXPORTS} */
+export { default as Log } from './Log'
+export { default as Events } from './Events'
+export { default as Platform } from './Platform'
+export { default as Settings } from './Settings'
