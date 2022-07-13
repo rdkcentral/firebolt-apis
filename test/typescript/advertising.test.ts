87651f955b6da47795b35212a8b0aeba83916c91
Vikas Warbhe <amit.vikas.warbhe@accenture.com>
2022-07-13 12:04:01 +0530

    adding typescript files

---
+++ test/typescript/advertising.test.ts
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2021 Comcast Cable Communications Management, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { test, expect } from '@jest/globals';
+import { Advertising } from '../../dist/lib/firebolt';
+
+test('policy()', () => {
+  return Advertising.policy().then((policy: Advertising.AdPolicy) => {
+    expect(policy.skipRestriction).toBe('adsUnwatched');
+    expect(policy.limitAdTracking).toBe(false);
+  });
+});
+
+test('config()', () => {
+  return Advertising.config({
+    coppa: true,
+    environment: 'test',
+    authenticationEntity: 'Test',
+  }).then((res: object) => {
+    expect(typeof res).toBe('object');
+  });
+});
+
+test('advertisingId()', () => {
+  return Advertising.advertisingId().then((res: object) => {
+    expect(typeof res).toBe('object');
+  });
+});
+
+test('deviceAttributes()', () => {
+  return Advertising.deviceAttributes().then((res: object) => {
+    expect(typeof res).toBe('object');
+  });
+});
+
+test('appBundleId()', () => {
+  return Advertising.appBundleId().then((res: string) => {
+    expect(res).toBe('operator.app');
+    expect(typeof res).toBe('string');
+  });
+});
+
+test('listen()', () => {
+  return Advertising.listen((event: string, data: object) => {}).then(
+    (res: number) => {
+      expect(res).toBe(2);
+    }
+  );
+});
+
+test('once()', () => {
+  return Advertising.once((event: string, data: object) => {}).then(
+    (res: number) => {
+      expect(res).toBe(3);
+    }
+  );
+});
+
+test('clear()', () => {
+  const result: boolean = Advertising.clear(2);
+  expect(result).toBeFalsy();
+});
---
+++ test/typescript/localization.test.ts
@@ -0,0 +1,37 @@
+import { test, expect } from "@jest/globals";
+import { Localization } from "../../dist/lib/firebolt";
+test("locality", () => {
+  return Localization.locality().then((res: string) => {
+    expect(res).toBe("Philadelphia");
+  });
+});
+test("postalCode", () => {
+  return Localization.postalCode().then((res: string) => {
+    expect(res).toBe("19103");
+  });
+});
+test("countryCode", () => {
+  return Localization.countryCode().then((res: string) => {
+    expect(res).toBe("US");
+  });
+});
+test("language", () => {
+  return Localization.language().then((res: string) => {
+    expect(res).toBe("en");
+  });
+});
+test("locale", () => {
+  return Localization.locale().then((res: string) => {
+    expect(res).toBe("en-US");
+  });
+});
+test("additionalInfo", () => {
+  return Localization.additionalInfo().then((res: object) => {
+    expect(typeof res).toBe("object");
+  });
+});
+test("latlon", () => {
+  return Localization.latlon().then((res: Array<Number>) => {
+    expect([39.9549, 75.1699]).toEqual(expect.arrayContaining(res));
+  });
+});
