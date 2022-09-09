ef12ec083591837e9a0c31d993f4e916810b11bc
Clay Simmons <clay_simmons@comcast.com>
2022-09-09 11:28:51 -0500

    fix(device): add device version firmware property
    * Add properties firmware and api to `Device.version()`
    * Deprecates Device.version().os, which is implemented inconsistently across Firebolt impls.
    
    #54 
    
    Co-authored-by: Jeremy LaCivita <jeremy.lacivita@comcast.com>

--- test/typescript/device.test.ts
+++ test/typescript/device.test.ts
@@ -21,14 +21,26 @@ import { Device } from "../../dist/lib/firebolt";
 
 test("Device.version()", () => {
   const expectedOutput: object = {
-    debug: "",
-    os: { major: 0, minor: 1, patch: 0, readable: "Firebolt OS v0.1.0" },
+    debug: "Non-parsable build info for error logging only.",
+    firmware: { major: 1, minor: 2, patch: 3, readable: "Device Firmware v1.2.3" },
     sdk: {
       major: 0,
       minor: 8,
       patch: 0,
       readable: "The Firebolt JS SDK",
     },
+    api: {
+      major: 0,
+      minor: 8,
+      patch: 0,
+      readable: "Firebolt API v0.8.0"
+    },
+    os: {
+      major: 0,
+      minor: 1,
+      patch: 0,
+      readable: "Firebolt OS v0.1.0"
+    }    
   };
   return Device.version().then((v: object) => {
     expect(v).toEqual(expectedOutput);
