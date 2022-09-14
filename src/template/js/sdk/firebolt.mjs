7cf412819de8bc9749d51af2de923556924ae564
Jeremy LaCivita <jeremy.lacivita@comcast.com>
2022-09-14 15:22:51 -0400

    feat(extensions): extensions module
    * Export new Extensions module #46 
    * Call new Capabilities.authorize API
    * Fallback to detecting commerce capabilities and calling approvePurchase

--- src/template/js/sdk/firebolt.mjs
+++ src/template/js/sdk/firebolt.mjs
@@ -17,6 +17,9 @@
  */
 
 import { setMockResponses } from './Transport/MockTransport.mjs'
+import { registerAPI } from './Extensions/index.mjs'
+import Authentication from './Authentication/index.mjs'
+import Transport from './Transport/index.mjs'
 
 /* ${MOCK_IMPORTS} */
 
@@ -24,7 +27,48 @@ setMockResponses({
   /* ${MOCK_OBJECTS} */
 })
 
+registerAPI('authorize', (required, optional) => {
+  return new Promise( (resolve, reject) => {
+    // this will fail until we support capabilities
+    // once it works, this will trigger user grant UIs, and update the FAT
+    Transport.send("capabilities", "authorize", {required, optional}).then(token => {
+      resolve(token)
+    })
+    // This is temporary. Will be handled by a user grant policy in future
+    .catch(error => {
+      // TODO: Need to finalize this error schema.
+      // currently assuming that any error from `capabilities.authorize` that has `data.capabilities`
+      // means that the method exists, and this is a list of capability info objects
+      if (error.data && error.data.capabilities) {
+        // reject, because something went wrong
+        reject(error)
+      }
+      // otherwise, this is a method not found error, and we should fallback to 0.x behavior
+      else {
+        // assume all commerce capabilities require a pin prompt
+        if (required.find(p => p.capability.startsWith('xrn:firebolt:capabilities:commerce:'))) {
+          Transport.send('profile', 'approvePurchase', {})
+            .then(result => {
+              if (result) {
+                Authentication.token('platform').then(token => {
+                  resolve(token.value)
+                })
+              }
+              else {
+                reject(new Error('User did not grant access to purchase.'))
+              }
+            })
+            .catch(error => {
+              reject(error)
+            })
+        }
+      }
+    })
+  })
+})
+
 /* ${EXPORTS} */
 export { default as Log } from './Log/index.mjs'
 export { default as Events } from './Events/index.mjs'
 export { default as Settings } from './Settings/index.mjs'
+export { default as Extensions } from './Extensions/index.mjs'
