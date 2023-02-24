21e6309e255b005906854119c38052d2064cd84c
LaCivita, Jeremy <jeremy.lacivita@comcast.com>
2023-02-24 16:04:41 -0500

    chore(discovery): Added disco sdk
    also chore(files): Remove artifacts from github

--- src/sdks/core/test/transpiled-suite/settings.test.js
+++
@@ -1,6 +0,0 @@
-import { test, expect } from "@jest/globals";
-import { Settings } from "../../build/javascript/src/firebolt";
-test('Log level tests', () => {
-    Settings.setLogLevel("DEBUG");
-    expect(Settings.getLogLevel()).toBe("DEBUG");
-});
