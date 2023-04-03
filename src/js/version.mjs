691f2fe02a1a7f87381cc2d32ca55083d653ec6e
Jeremy LaCivita <jeremy.lacivita@comcast.com>
2023-04-03 14:07:51 -0400

    chore(release): Don't rebuild entire repository for each SDK

--- src/js/version.mjs
+++ src/js/version.mjs
@@ -8,10 +8,21 @@ process.argv.shift()
 
 const task = process.argv.shift()
 
+let inWorkspace = false
+let packageJson = await readFile(path.join('.', 'package.json'))
+                            .then(bytes => bytes.toString())
+                            .then(JSON.parse)
+
+if (!packageJson.workspaces) {
+    inWorkspace = true
+    packageJson = await readFile(path.join('..', '..', '..', 'package.json'))
+                            .then(bytes => bytes.toString())
+                            .then(JSON.parse)
+}
+
+const packageMain = packageJson
+
 if (task === 'sync') {
-    const packageMain = await readFile(path.join('.', 'package.json'))
-        .then(bytes => bytes.toString())
-        .then(JSON.parse)
     
     console.log()
     
@@ -35,4 +46,31 @@ else if (task === 'channel') {
     const config = releaserc.branches.find(b => b.name === branch) || { channel: 'test' }
 
     console.log(config.channel)
+}
+else if (task === 'validate') {
+    // Check all workspaces for:
+    //  - package.json.version is the same
+    //  - pacakge.json.main exists
+    //  - package.json.types exists
+    await Promise.all(packageMain.workspaces.map(async workspace => {
+        console.log(`Validating ${workspace} distribution matches version.`)
+        const packagePath = inWorkspace ? path.join('.') : path.join('.', workspace)
+        const packageWorkspace = await readFile(path.join(packagePath, 'package.json'))
+            .then(bytes => bytes.toString())
+            .then(JSON.parse)
+        if (packageWorkspace.version !== packageMain.version) {
+            console.error(`${packageWorkspace.name} version does not match root.`)
+            process.exit(-1)
+        }
+
+        const main = await readFile(path.join(packagePath, packageWorkspace.main))
+        const types = await readFile(path.join(packagePath, packageWorkspace.types))
+
+        if (!(main.byteLength > 0 && types.byteLength > 0)) {
+            console.error(`${packageWorkspace.name} has empty artifacts.`)
+            process.exit(-1)
+        }
+    }))
+
+
 }
