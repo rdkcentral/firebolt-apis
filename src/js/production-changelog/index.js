4530cd53cb1a6119d1bb10c169c8c45f06898ce2
Jeremy LaCivita <jeremy.lacivita@comcast.com>
2023-04-04 14:26:52 -0400

    chore(release): Cleaning up code

--- src/js/production-changelog/index.js
+++ src/js/production-changelog/index.js
@@ -1,42 +1,36 @@
 const path = require('path');
 const { readFile, writeFile, ensureFile } = require('fs-extra');
 
-const resolveConfig = ({ changelogFile, changelogTitle }) => ({
+const getConfig = ({ changelogFile, changelogTitle }) => ({
     changelogFile: !changelogFile ? 'CHANGELOG.md' : changelogFile,
     changelogTitle,
 });
 
-async function prepare(pluginConfig, { cwd, nextRelease: { notes }, logger }) {
-    const { changelogFile, changelogTitle } = resolveConfig(pluginConfig);
-    const changelogPath = path.resolve(cwd, changelogFile);
+async function prepare(config, { cwd, nextRelease: { notes }, logger }) {
+    const { changelogFile, changelogTitle } = getConfig(config)
+    const logPath = path.resolve(cwd, changelogFile)
 
     if (notes) {
-        await ensureFile(changelogPath);
-        const currentFile = (await readFile(changelogPath)).toString().trim();
+        await ensureFile(logPath)
+        const currentFile = (await readFile(logPath)).toString().trim()
 
-        if (currentFile) {
-            logger.log('Update %s', changelogPath);
-        } else {
-            logger.log('Create %s', changelogPath);
-        }
-
-        let currentContent =
+        let content =
             changelogTitle && currentFile.startsWith(changelogTitle)
                 ? currentFile.slice(changelogTitle.length).trim()
                 : currentFile;
 
-        logger.log("Scrubbing prerelease notes.")
+        logger.log(`Scrubbing prerelease notes from ${logPath}.`)
 
         // Looking for things like:
         //# [0.10.0-next.5](https://github.com/rdkcentral/firebolt-core-sdk/compare/v0.10.0-next.4...v0.10.0-next.5) (2023-02-01)
         const regex = /\# \[[0-9]+\.[0-9]+\.[0-9]+\-[^\]]+\].*?\n+\# /gms
-        while (currentContent.match(regex)) {
-            currentContent = currentContent.replace(regex, '\n# ')
+        while (content.match(regex)) {
+            content = content.replace(regex, '\n# ')
         }
 
-        let content = `${notes.trim()}\n${currentContent ? `\n${currentContent}\n` : ''}`;
+        content = `${notes.trim()}\n${content ? `\n${content}\n` : ''}`;
 
-        await writeFile(changelogPath, changelogTitle ? `${changelogTitle}\n\n${content}` : content);
+        await writeFile(logPath, changelogTitle ? `${changelogTitle}\n\n${content}` : content);
     }
 }
 
