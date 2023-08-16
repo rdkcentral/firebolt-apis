a912cb2714fe09433068987794abcc5a1c9d0ddf
HaseenaSainul <41037131+HaseenaSainul@users.noreply.github.com>
2023-08-16 19:26:10 +0530

    native tests added (#140)
    * native tests added
    
    * review updates
    
    * Test app alignment based on change in the static/generated code
    
    * ctest: add WPEFrameworkCore also in the dependency
    
    * Native test update based on interface return type change from uint32_t to int32_t

---
+++ src/sdks/core/src/cpp/sdk/test/CoreSDKTestGeneratedCode.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2023 Comcast Cable Communications Management, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef CORESDK_TEST_GENERATED_CODE_H
+#define CORESDK_TEST_GENERATED_CODE_H
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//int32_t test_generated_properties_get_device_version();
+int32_t test_generated_properties_get_device_id();
+int32_t test_generated_properties_get_accessibility_closedcaption();
+int32_t test_generated_properties_get_accessibility_voiceguidancesettings();
+int32_t test_generated_properties_get_advertising_policy();
+int32_t test_generated_event_device_name();
+int32_t test_generated_event_device_name_with_register_same_callback();
+int32_t test_generated_event_device_screenresolution();
+int32_t test_generated_event_accessibility_voice_guidance_settings();
+//int32_t test_generated_calls_metrics_lifecycle_ready();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // CORESDK_TEST_GENERATED_CODE_H
