a49dfbb68bff82149a601ccf3be53f3684c10cec
Shah, Kevin <Kevin_Shah@comcast.com>
2023-11-06 10:49:25 -0500

    Added branch name in .releaserc

--- src/js/github.io/index.mjs
+++ src/js/github.io/index.mjs
@@ -176,13 +176,13 @@ function channel(version) {
     if (parts.length > 1) {
         parts.shift()
         const chnl = parts.join("-").split(".").shift()
-
-        if (['next', 'next-major', 'test'].includes(chnl)) {
-            return chnl
-        }
-        else {
-            return 'test'
-        }
+        return chnl
+        // if (['next', 'next-major', 'test'].includes(chnl)) {
+        //     return chnl
+        // }
+        // else {
+        //     return 'test'
+        // }
     }
     else {
         return 'latest'
