b50e8434536d16d85acb0aaa8d6ee7e7ec5f1c0a
AdityaKasar <adityakasar1994@gmail.com>
2024-08-12 12:22:19 +0530

    Unit test setup

--- src/sdks/core/src/cpp/sdk/cpptest/build.sh
+++ src/sdks/core/src/cpp/sdk/cpptest/build.sh
@@ -6,6 +6,7 @@ usage()
    echo "    -s sysroot path"
    echo "    -f firebolt path"
    echo "    -c clear build"
+   echo "    -u build unit tests"
    echo "    -h : help"
    echo
    echo "usage: "
@@ -16,13 +17,14 @@ TestPath="."
 FireboltPath=${FIREBOLT_PATH}
 SysrootPath=${SYSROOT_PATH}
 ClearBuild="N"
-while getopts t:s:f:ch flag
+while getopts t:s:f:c:uh flag
 do
     case "${flag}" in
         t) TestPath="${OPTARG}";;
         s) SysrootPath="${OPTARG}";;
         f) FireboltPath="${OPTARG}";;
         c) ClearBuild="Y";;
+        u) UnitTest="ON";;
         h) usage && exit 1;;
     esac
 done
@@ -36,6 +38,6 @@ echo "TestPath"
 echo "${TestPath}"
 echo "FireboltPath"
 echo ${FireboltPath}
-cmake -B${TestPath}/build -S${TestPath} -DSYSROOT_PATH=${SysrootPath} -DFIREBOLT_PATH=${FireboltPath}
+cmake -B${TestPath}/build -S${TestPath} -DSYSROOT_PATH=${SysrootPath} -DFIREBOLT_PATH=${FireboltPath} -DUNIT_TEST=${UnitTest}
 # -DPOLYMORPHICS_METHODS=ON // Enable this to test 
-cmake --build ${TestPath}/build
+cmake --build ${TestPath}/build 
