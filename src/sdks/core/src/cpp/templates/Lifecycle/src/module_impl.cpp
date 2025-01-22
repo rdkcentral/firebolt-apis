4a29c0099d0d4a34ac56d68770cf6f25ded3f9e5
Tomasz Blaszczak <tomaszblaszczak.w2@outlook.com>
2025-01-22 13:08:14 +0100

    feat: Distinction between uni- and bi-directional lifecycle
    Specification for Lifecycle 2.0 is not yet ready, thus the change is just
    splitting lifecycle into two parts where the part for Lifecycle 2.0 is
    currently exactly the same as existing one.

--- src/sdks/core/src/cpp/templates/Lifecycle/src/module_impl.cpp
+++ src/sdks/core/src/cpp/templates/Lifecycle/src/module_impl.cpp
@@ -33,6 +33,7 @@ EnumMap<Firebolt::Lifecycle::LifecycleState> lifecycleStateMap = {
     { Firebolt::Lifecycle::LifecycleState::SUSPENDED, "suspended" }
 };
 
+#ifdef GATEWAY_BIDIRECTIONAL
 
 /* ready - Notify the platform that the app is ready */
 static void readyDispatcher(const void* result) {
@@ -135,6 +136,95 @@ void ${info.Title}Impl::finished(Firebolt::Error *err)
 
     return;
 }
+
+#else
+
+/* ready - Notify the platform that the app is ready */
+static void readyDispatcher(const void* result) {
+    Firebolt::IFireboltAccessor::Instance().MetricsInterface().ready();
+}
+
+// localCallback to update the state
+static void onReadyInnerCallback(void* notification, const void* userData, void* jsonResponse)
+{
+    const LifecycleImpl* selfConst = static_cast<const LifecycleImpl*>(userData);
+    LifecycleImpl* self = const_cast<LifecycleImpl*>(selfConst);
+
+    WPEFramework::Core::ProxyType<JsonData_LifecycleEvent>& proxyResponse = *(reinterpret_cast<WPEFramework::Core::ProxyType<JsonData_LifecycleEvent>*>(jsonResponse));
+    ASSERT(proxyResponse.IsValid() == true);
+
+    if (proxyResponse.IsValid() == true) {
+        LifecycleEvent value;
+
+        value.state = proxyResponse->State;
+        std::string stateStr = ConvertEnum<Firebolt::Lifecycle::LifecycleState>(lifecycleStateMap, value.state);
+        // Assign stateStr to currentState in ${info.Title}Impl instance
+        self->currentState = stateStr;
+        std::cout << "Updated the Current State to: " << self->currentState << std::endl;
+
+        proxyResponse.Release();
+
+    }
+}
+
+/* ready - Notify the platform that the app is ready */
+void ${info.Title}Impl::ready(Firebolt::Error *err) {
+    Firebolt::Error status = Firebolt::Error::NotConnected;
+
+    JsonObject jsonParameters;
+
+    // Call Prioritize to subscribe to corresponding event, add to internalMap, and prioritize its callback
+    status = FireboltSDK::Event::Instance().Prioritize<JsonData_LifecycleEvent>("lifecycle.onForeground", jsonParameters, onReadyInnerCallback, (void*)nullptr, this);
+    status = FireboltSDK::Event::Instance().Prioritize<JsonData_LifecycleEvent>("lifecycle.onBackground", jsonParameters, onReadyInnerCallback, (void*)nullptr, this);
+    status = FireboltSDK::Event::Instance().Prioritize<JsonData_LifecycleEvent>("lifecycle.onInactive", jsonParameters, onReadyInnerCallback, (void*)nullptr, this);
+    status = FireboltSDK::Event::Instance().Prioritize<JsonData_LifecycleEvent>("lifecycle.onSuspended", jsonParameters, onReadyInnerCallback, (void*)nullptr, this);
+    status = FireboltSDK::Event::Instance().Prioritize<JsonData_LifecycleEvent>("lifecycle.onUnloading", jsonParameters, onReadyInnerCallback, (void*)nullptr, this);
+
+    WPEFramework::Core::JSON::VariantContainer jsonResult;
+    status = FireboltSDK::Gateway::Instance().Request("lifecycle.ready", jsonParameters, jsonResult);
+    if (status == Firebolt::Error::None) {
+        FIREBOLT_LOG_INFO(FireboltSDK::Logger::Category::OpenRPC, FireboltSDK::Logger::Module<FireboltSDK::Accessor>(), "Lifecycle.ready is successfully invoked");
+
+        WPEFramework::Core::ProxyType<WPEFramework::Core::IDispatch> job = WPEFramework::Core::ProxyType<WPEFramework::Core::IDispatch>(WPEFramework::Core::ProxyType<FireboltSDK::Worker>::Create(readyDispatcher, nullptr));
+        WPEFramework::Core::IWorkerPool::Instance().Submit(job);
+    } else {
+        FIREBOLT_LOG_ERROR(FireboltSDK::Logger::Category::OpenRPC, FireboltSDK::Logger::Module<FireboltSDK::Accessor>(), "Error in invoking lifecycle.ready: %d", status);
+    }
+
+    if (err != nullptr) {
+        *err = status;
+    }
+}
+
+/* state - return the state of the app */
+std::string ${info.Title}Impl::state(Firebolt::Error *err)  {
+    std::cout << "**CURRENT STATE OF THE APP::::**" << currentState << std::endl;
+    return currentState;
+}
+
+/* finished - Notify the platform that the app is done unloading */
+void ${info.Title}Impl::finished(Firebolt::Error *err)
+{
+    Firebolt::Error status = Firebolt::Error::NotConnected;
+    if(currentState == "unloading")
+    {
+        JsonObject jsonParameters;
+
+        WPEFramework::Core::JSON::VariantContainer jsonResult;
+        status = FireboltSDK::Gateway::Instance().Request("lifecycle.finished", jsonParameters, jsonResult);
+        if (status == Firebolt::Error::None) {
+            FIREBOLT_LOG_INFO(FireboltSDK::Logger::Category::OpenRPC, FireboltSDK::Logger::Module<FireboltSDK::Accessor>(), "Lifecycle.finished is successfully invoked");
+        }
+
+        if (err != nullptr) {
+            *err = status;
+        }
+    }
+
+    return;
+}
+
+#endif
 
 // Methods
 /* ${METHODS} */
