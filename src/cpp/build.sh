8c3723aaff6f2713ad196aa11da04dcb97d6d2cd
Tomasz Blaszczak <tomaszblaszczak.w2@outlook.com>
2025-06-11 10:38:53 +0200

    feat: Enable Demo App from the main cmake

--- src/cpp/build.sh
+++ src/cpp/build.sh
@@ -2,14 +2,18 @@
 
 set -e
 
-SYSROOT_PATH=${1:-$SYSROOT_PATH}
+case $1 in
+'') ;;
+--) shift;;
+*) SYSROOT_PATH="$1"; shift;;
+esac
 [[ ! -z $SYSROOT_PATH ]] || { echo "SYSROOT_PATH not set" >/dev/stderr; exit 1; }
 [[ -e $SYSROOT_PATH ]] || { echo "SYSROOT_PATH not exist ($SYSROOT_PATH)" >/dev/stderr; exit 1; }
 
 bdir="build"
 idir="install"
 rm -rf $bdir $idir
-cmake -B $bdir -DSYSROOT_PATH=$SYSROOT_PATH
+cmake -B $bdir -DSYSROOT_PATH=$SYSROOT_PATH "$@"
 cmake --build $bdir
 cmake --install $bdir
 
