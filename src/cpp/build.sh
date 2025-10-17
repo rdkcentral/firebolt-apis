5945700446d2220c5fbf5b881db2f74295ce3c32
Tomasz Blaszczak <tomasz.blaszcak@sky.uk>
2025-10-17 13:22:31 +0200

    build.sh: configure build type

--- src/cpp/build.sh
+++ src/cpp/build.sh
@@ -6,23 +6,32 @@ bdir="build"
 idir="install"
 do_install=false
 params=
+buildTarget="Debug"
 
 while [[ ! -z $1 ]]; do
   case $1 in
   --clean) rm -rf $bdir $idir;;
+  --release) buildTarget="Release";;
+  --sysroot) SYSROOT_PATH="$2"; shift;;
+  -i | --install) do_install=true;;
   +ut)    params+=" -DENABLE_UNIT_TESTS=ON";;
   +tests) params+=" -DENABLE_TESTS=ON";;
   +demo)  params+=" -DENABLE_DEMO_APP=ON";;
-  -i | --install) do_install=true;;
   --) shift; break;;
-  *) SYSROOT_PATH="$1"; shift;;
+  *) break;;
   esac; shift
 done
 
 [[ ! -z $SYSROOT_PATH ]] || { echo "SYSROOT_PATH not set" >/dev/stderr; exit 1; }
 [[ -e $SYSROOT_PATH ]] || { echo "SYSROOT_PATH not exist ($SYSROOT_PATH)" >/dev/stderr; exit 1; }
 
-[[ -e $bdir ]] || cmake -B $bdir -DSYSROOT_PATH=$SYSROOT_PATH $params "$@"
+if [[ ! -e $bdir ]]; then
+  cmake -B $bdir \
+    -DCMAKE_BUILD_TYPE=$buildType \
+    -DSYSROOT_PATH=$SYSROOT_PATH \
+    $params \
+    "$@"
+fi
 cmake --build $bdir || exit $?
 if $do_install; then
   cmake --install $bdir || exit $?
--- src/cpp/demo/build.sh
+++ src/cpp/demo/build.sh
@@ -4,28 +4,36 @@ set -e
 
 bdir="build"
 run=true
+buildTarget="Debug"
 
 while [[ ! -z $1 ]]; do
   case $1 in
   --clean) rm -rf $bdir;;
-  --no-run) run=false;;
   --just-run)
-    export LD_LIBRARY_PATH=$SYSROOT_PATH/usr/lib:$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH=$bdir/src:$SYSROOT_PATH/usr/lib:$LD_LIBRARY_PATH
     $bdir/FireboltCoreSDKDemo
     exit;;
+  --no-run) run=false;;
+  --release) buildTarget="Release";;
+  --sysroot) SYSROOT_PATH="$2"; shift;;
   --) shift; break;;
-  *) SYSROOT_PATH="$1"; shift;;
+  *) break;;
   esac; shift
 done
 
 [[ ! -z $SYSROOT_PATH ]] || { echo "SYSROOT_PATH not set" >/dev/stderr; exit 1; }
 [[ -e $SYSROOT_PATH ]] || { echo "SYSROOT_PATH not exist ($SYSROOT_PATH)" >/dev/stderr; exit 1; }
 
-[[ -e $bdir ]] || cmake -B $bdir -DSYSROOT_PATH=$SYSROOT_PATH "$@"
+if [[ ! -e $bdir ]]; then
+  cmake -B $bdir \
+    -DCMAKE_BUILD_TYPE=$buildType \
+    -DSYSROOT_PATH=$SYSROOT_PATH \
+    "$@"
+fi
 cmake --build $bdir
 
 if $run; then
-  export LD_LIBRARY_PATH=$SYSROOT_PATH/usr/lib:$LD_LIBRARY_PATH
+  export LD_LIBRARY_PATH=$bdir/src:$SYSROOT_PATH/usr/lib:$LD_LIBRARY_PATH
   $bdir/FireboltCoreSDKDemo
 fi
 
