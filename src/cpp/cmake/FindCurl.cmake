5d63166a8aef0e26d3bfdf7a7099edbd8580d90e
Vladimir Rangelov <vladimir.rangelov@gmail.com>
2025-10-27 09:20:34 +0000

    refactor: Refactor FindCurl.cmake

--- src/cpp/cmake/FindCurl.cmake
+++ src/cpp/cmake/FindCurl.cmake
@@ -1,32 +1,56 @@
-# - Try to find the glib 2.0 library.
+# - FindCurl.cmake
+# Finds the CURL library and includes
+# This will define:
 #
-# The following are set after configuration is done:
-#  CURL_FOUND
-#  CURL_INCLUDE_DIRS
-#  CURL_LIBRARY_DIRS
-#  CURL_LIBRARIES
+#   CURL_FOUND          - TRUE if CURL is found
+#   CURL_INCLUDE_DIRS   - The directory where curl/curl.h is found
+#   CURL_LIBRARIES      - The path to the CURL library
+#   CURL_VERSION_STRING - Detected version of libcurl
+#
+# and will define the imported target:
+#
+#   CURL::libcurl
 
-find_path( CURL_INCLUDE_DIR NAMES curl/curl.h )
-find_library( CURL_LIBRARY NAMES curl libcurl  )
+find_path(CURL_INCLUDE_DIR
+    NAMES curl/curl.h
+    PATHS
+        /usr/include
+        /usr/local/include
+        /opt/local/include
+)
 
-# message( "CURL_INCLUDE_DIR include dir = ${CURL_INCLUDE_DIR}" )
-# message( "CURL_LIBRARY lib = ${CURL_LIBRARY}" )
+find_library(CURL_LIBRARY
+    NAMES curl libcurl
+    PATHS
+        /usr/lib
+        /usr/local/lib
+        /usr/lib/x86_64-linux-gnu
+        /opt/local/lib
+)
 
-include( FindPackageHandleStandardArgs )
-
-# Handle the QUIETLY and REQUIRED arguments and set the CURL_FOUND to TRUE
-# if all listed variables are TRUE
-find_package_handle_standard_args( CURL DEFAULT_MSG
-        CURL_LIBRARY CURL_INCLUDE_DIR )
-
-mark_as_advanced( CURL_INCLUDE_DIR CURL_LIBRARY )
-
-if( CURL_FOUND AND NOT TARGET CURL::libcurl)
-    set( CURL_LIBRARIES ${CURL_LIBRARY} )
-    set( CURL_INCLUDE_DIRS ${CURL_INCLUDE_DIR} )
-
-    add_library( CURL::libcurl SHARED IMPORTED )
-    set_target_properties( CURL::libcurl PROPERTIES
-            IMPORTED_LOCATION "${CURL_LIBRARY}"
-            INTERFACE_INCLUDE_DIRECTORIES "${CURL_INCLUDE_DIRS}" )
+# Check version
+if (CURL_INCLUDE_DIR AND EXISTS "${CURL_INCLUDE_DIR}/curl/curlver.h")
+    file(STRINGS "${CURL_INCLUDE_DIR}/curl/curlver.h" _curl_version_str
+        REGEX "^#define[ \t]+LIBCURL_VERSION[ \t]+\"[^\"]+\"")
+    string(REGEX REPLACE "^#define[ \t]+LIBCURL_VERSION[ \t]+\"([^\"]+)\".*" "\\1"
+        CURL_VERSION_STRING "${_curl_version_str}")
 endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Curl
+    REQUIRED_VARS CURL_LIBRARY CURL_INCLUDE_DIR
+    VERSION_VAR CURL_VERSION_STRING)
+
+if (CURL_FOUND)
+    set(CURL_LIBRARIES ${CURL_LIBRARY})
+    set(CURL_INCLUDE_DIRS ${CURL_INCLUDE_DIR})
+
+    if (NOT TARGET CURL::libcurl)
+        add_library(CURL::libcurl UNKNOWN IMPORTED)
+        set_target_properties(CURL::libcurl PROPERTIES
+            IMPORTED_LOCATION "${CURL_LIBRARY}"
+            INTERFACE_INCLUDE_DIRECTORIES "${CURL_INCLUDE_DIR}")
+    endif()
+endif()
+
+mark_as_advanced(CURL_INCLUDE_DIR CURL_LIBRARY)
