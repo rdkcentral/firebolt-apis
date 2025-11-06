c6b6fcfdd93658282a4336a2715747028039bcc6
Vladimir Rangelov <vladimir.rangelov@gmail.com>
2025-11-06 09:26:45 +0000

    Add lifecycle2 component tests (#438)
    * test: Add lifecycle2 component tests
    
    * refactor: Rename state event to stateChanged
    
    * test: Fix names in UTs
    
    ---------
    
    Co-authored-by: Tomasz Blaszczak <tomasz.blaszcak@sky.uk>

---
+++ src/cpp/test/component/lifecycle2Test.cpp
@@ -0,0 +1,86 @@
+/*
+ * If not stated otherwise in this file or this component's LICENSE file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2025 Sky UK
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "./component/utils.h"
+#include "firebolt.h"
+#include "json_engine.h"
+#include "gtest/gtest.h"
+
+#include <condition_variable>
+#include <iostream>
+#include <mutex>
+#include <thread>
+
+class LifecycleTest : public ::testing::Test
+{
+protected:
+    void SetUp() override { eventReceived = false; }
+
+    // Create a condition variable and mutex to wait for the events to be received
+    std::condition_variable cv;
+    std::mutex mtx;
+    bool eventReceived;
+
+    JsonEngine jsonEngine;
+};
+
+TEST_F(LifecycleTest, close)
+
+{
+    auto result =
+        Firebolt::IFireboltAccessor::Instance().LifecycleInterface().close(Firebolt::Lifecycle::CloseType::DEACTIVATE);
+    ASSERT_TRUE(result) << "Error on calling LifecycleInterface.close() method";
+}
+
+TEST_F(LifecycleTest, state)
+{
+    Firebolt::Result<Firebolt::Lifecycle::LifecycleState> result =
+        Firebolt::IFireboltAccessor::Instance().LifecycleInterface().getCurrentState();
+    ASSERT_TRUE(result) << "Error on calling LifecycleInterface.getCurrentState() method";
+
+    auto expectedValue = jsonEngine.get_value("Lifecycle.state");
+    EXPECT_EQ(*result, Firebolt::Lifecycle::LifecycleState::ACTIVE);
+}
+
+TEST_F(LifecycleTest, subscribeOnState)
+{
+    auto id = Firebolt::IFireboltAccessor::Instance().LifecycleInterface().subscribeOnStateChanged(
+        [&](const auto &oldState, const auto &newState)
+        {
+            std::cout << "[Subscription] Lifecycle state changed: " << static_cast<int>(newState)
+                      << ", old state: " << static_cast<int>(oldState) << std::endl;
+
+            EXPECT_EQ(newState, Firebolt::Lifecycle::LifecycleState::PAUSED);
+            EXPECT_EQ(oldState, Firebolt::Lifecycle::LifecycleState::INITIALIZING);
+
+            {
+                std::lock_guard<std::mutex> lock(mtx);
+                eventReceived = true;
+            }
+            cv.notify_one();
+        });
+    verifyEventSubscription(id);
+    // Trigger the event from the mock server
+    triggerEvent("Device.onStateChanged", R"({ "newState": "active", "oldState": "paused" })");
+
+    verifyEventReceived(mtx, cv, eventReceived);
+    // Unsubscribe from the event
+    auto result = Firebolt::IFireboltAccessor::Instance().DeviceInterface().unsubscribe(id.value());
+    verifyUnsubscribeResult(result);
+}
