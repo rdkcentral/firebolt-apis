ae34e2d1378e0c0df838735415108787ce7b0bdd
Tomasz Blaszczak <tomasz.blaszcak@sky.uk>
2025-12-15 13:43:10 +0100

    build: When tests enabled, link against compiled lib, not installed one

--- src/cpp/build.sh
+++ src/cpp/build.sh
@@ -6,18 +6,19 @@ bdir="build"
 do_install=false
 params=
 buildType="Debug"
+cleanFirst=false
 
 while [[ ! -z $1 ]]; do
   case $1 in
-  --clean) rm -rf $bdir;;
+  --clean) cleanFirst=true;;
   --release) buildTarget="Release";;
   --sysroot) SYSROOT_PATH="$2"; shift;;
   -i | --install) do_install=true;;
-  +tests) params+=" -DENABLE_TESTS=ON";;
+  +tests) params+=" -DENABLE_TESTS=ON"; bdir="build-dev";;
   +demo)  params+=" -DENABLE_DEMO_APP=ON";;
   +gen-cov)
     set -e
-    cd build
+    cd build-dev
     cp ../firebolt-open-rpc.json ./test/
     ctest --test-dir ./test
     mkdir -p coverage
@@ -37,15 +38,16 @@ done
 [[ ! -z $SYSROOT_PATH ]] || { echo "SYSROOT_PATH not set" >/dev/stderr; exit 1; }
 [[ -e $SYSROOT_PATH ]] || { echo "SYSROOT_PATH not exist ($SYSROOT_PATH)" >/dev/stderr; exit 1; }
 
+$cleanFirst && rm -rf $bdir
+
 if [[ ! -e $bdir ]]; then
   cmake -B $bdir \
     -DCMAKE_BUILD_TYPE=$buildType \
     -DSYSROOT_PATH=$SYSROOT_PATH \
     $params \
-    "$@"
+    "$@" || exit $?
 fi
 cmake --build $bdir --parallel || exit $?
-if $do_install; then
+if $do_install && [[ $bdir == 'build' ]]; then
   cmake --install $bdir || exit $?
 fi
-
